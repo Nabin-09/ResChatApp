02c2ab1654f1c3a910ff539077435de330501c16
Nabin Sharma <sharmanabin.0910@gmail.com>
2025-07-05 18:00:57 +0530

    Redesigning Auth:

--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,37 +1,5 @@
-
-import React from "react";
-import Victory from '../../assets/victory.svg'
-import Background from '../../assets/login2.png'
-
 const Auth = () => {
-    return(
-        <div className="h-[100vh] w -[100vw] flex items-center justify-center">
+    return <div className="h-[100vh] w-[100vw] flex items-center justify-center">Auth</div>
+};
 
-            <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw]
-            md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
-
-                <div className="flex flex-col gap-10 items-center justify-center ">
-
-                    <div className="flex items-center justify-center flex-col">
-
-                        <div className="flex items-center justify-center">
-
-                            <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
-                            <img src={Victory} alt="Victory emoji" className="h-[100px]"/>
-
-                        </div>
-
-                        <p className="font-medium text-center">Fill in the details to start with the app</p>
-
-                    </div>
-                    <div className="flex items-center justify-center w-full"></div>
-                </div>
-
-            </div>    
-
-        </div>
-
-    )
-}
-
-export default Auth
+export default Auth;
